12b21068f7a487741b08aec60fcd3b94f277c013
staunsholm <crap2@staunsholm.dk>
2020-12-08 11:19:05 +0100

    translation value: show multiline value (string -> text)

--- studio/schemas/objects/localeString.js
+++ studio/schemas/objects/localeString.js
@@ -1,20 +1,21 @@
 import { languages } from '../languages';
 
 export default {
-    name: 'localeString',
-    type: 'object',
-    title: 'Translatable string',
-    fieldsets: [
-        {
-            title: 'Translations',
-            name: 'translations',
-            options: { collapsible: true },
-        },
-    ],
-    fields: languages.map((lang) => ({
-        title: lang.title,
-        name: lang.id,
-        type: 'string',
-        fieldset: lang.isDefault ? null : 'translations',
-    })),
+  name: 'localeString',
+  type: 'object',
+  title: 'Translatable string',
+  fieldsets: [
+    {
+      title: 'Translations',
+      name: 'translations',
+      options: { collapsible: true },
+    },
+  ],
+  fields: languages.map((lang) => ({
+    title: lang.title,
+    name: lang.id,
+    type: 'text',
+    rows: 1,
+    fieldset: lang.isDefault ? null : 'translations',
+  })),
 };
